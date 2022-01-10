882d063350707118091e5ad8904845f00549adfa
leo.wu <leo.wu@tm-robot.com>
2022-01-10 14:54:37 +0800

    add index

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,7 @@ author = 'leo'
 # The full version, including alpha/beta/rc tags
 release = 'v1'
 
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
